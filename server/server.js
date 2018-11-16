d8da00d08fb286aa941773801ccffef16118003e
Tzechiu Lei <tclei2009@gmail.com>
2018-11-15 18:04:36 -0700

    Refactor server.js fallback path.

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(history({
   verbose: true
 }));
 app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '../dist/index.html'));
+  res.render(path.join(__dirname + '..', 'dist', 'index.html'));
 });
 app.listen(port);
 
