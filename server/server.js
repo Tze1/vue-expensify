f7e70dc8c11c8f01deaf065d6d01dcfdca7e23e4
Tzechiu Lei <tclei2009@gmail.com>
2018-11-15 17:14:10 -0700

    Refactor server.js

--- server/server.js
+++ server/server.js
@@ -6,8 +6,14 @@ var serveStatic = require('serve-static');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.use(history());
 app.use(serveStatic(path.join(__dirname, '..', 'dist')));
+app.use(history({
+  disableDotRule: true,
+  verbose: true
+}));
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '../dist/index.html'));
+});
 app.listen(port);
 
 console.log('server started on port '+ port);
