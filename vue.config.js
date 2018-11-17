ca1973d7d0224e972c5ee272bf79d378362179cf
Tzechiu Lei <tclei2009@gmail.com>
2018-11-16 20:28:55 -0700

    Restore .env.production for hosted webserver deployments.

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,8 @@ module.exports = {
       require('dotenv').config({ path: '.env.test'});
     } else if (process.env.NODE_ENV === 'development') {
       require('dotenv').config({ path: '.env.development'});
+    } else if (process.env.NODE_ENV === 'production') {
+      require('dotenv').config({ path: '.env.production'});
     }
   },
   configureWebpack: {
