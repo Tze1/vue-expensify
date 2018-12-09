8e0095f7e559704c4ef67b8ebc76c9f3a2285abf
Tzechiu Lei <tclei2009@gmail.com>
2018-12-09 14:59:30 -0700

    Move all vue-fontawesome icon-imports to main.js.

--- src/main.js
+++ src/main.js
@@ -7,7 +7,14 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(faUserSecret);
+// Add all icons here for entire app.
+/* eslint-disable no-undef */
+library.add(
+  faEdit,
+  faSignOutAlt,
+  faTrashAlt,
+);
+/* eslint-enable no-undef */
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
